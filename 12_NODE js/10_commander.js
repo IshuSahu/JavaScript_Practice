35b5dc1a065fb2869e0f32b91d5513d09fdf31df
IshuSahu <ishusahu593@gmail.com>
2025-01-16 22:06:43 +0530

    Js commandar

---
+++ 12_NODE js/10_commander.js
@@ -0,0 +1,21 @@
+// const { Command } = require('commander');
+// const program = new Command();
+
+// program
+//   .name('my-cli')
+//   .description('A simple CLI tool')
+//   .version('1.0.0');
+
+// program
+//   .command('greet <name>')
+//   .description('Greet a user by name')
+//   .option('-e, --excited', 'Add excitement to the greeting')
+//   .action((name, options) => {
+//     const greeting = options.excited
+//       ? `Hello, ${name}!!!`
+//       : `Hello, ${name}.`;
+//     console.log(greeting);
+//   });
+
+// program.parse(process.argv);
+
